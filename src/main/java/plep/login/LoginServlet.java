6bb91d3430eaf1a36b18c8d2fa8993d72d774539
Dorian Gautier <gautier.dor@gmail.com>
2021-11-24 19:31:06 +0100

    LoginPage

---
+++ src/main/java/plep/login/LoginServlet.java
@@ -0,0 +1,71 @@
+package plep.login;
+
+
+
+import java.io.IOException;
+import plep.entite.Utilisateur;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public LoginServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        RequestDispatcher dispatcher //
+                = this.getServletContext().getRequestDispatcher("/WEB-INF/views/loginView.jsp");
+
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+        Utilisateur userAccount = .findUser(userName, password);
+
+        if (userAccount == null) {
+            String errorMessage = "Invalid userName or Password";
+
+            request.setAttribute("errorMessage", errorMessage);
+
+            RequestDispatcher dispatcher //
+                    = this.getServletContext().getRequestDispatcher("/WEB-INF/views/loginView.jsp");
+
+            dispatcher.forward(request, response);
+            return;
+        }
+
+        userAccount.storeLoginedUser(request.getSession(), userAccount);
+
+        //
+        int redirectId = -1;
+        try {
+            redirectId = Integer.parseInt(request.getParameter("redirectId"));
+        } catch (Exception e) {
+        }
+        String requestUri = AppUtils.getRedirectAfterLoginUrl(request.getSession(), redirectId);
+        if (requestUri != null) {
+            response.sendRedirect(requestUri);
+        } else {
+            // Par défaut, après l'achèvement de la connexion
+            // redirigez à la page /userInfo
+            response.sendRedirect(request.getContextPath() + "/userInfo");
+        }
+
+    }
+
+}
