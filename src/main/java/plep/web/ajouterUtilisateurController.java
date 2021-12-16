d6ba1bb4d5fc982e87711980e2fff807e58fd450
gabrielfiliot <gabyfil@yahoo.com>
2021-12-16 13:37:58 +0100

    verif username dans bdd et creation de compte

--- src/main/java/plep/web/ajouterUtilisateurController.java
+++ src/main/java/plep/web/ajouterUtilisateurController.java
@@ -2,15 +2,19 @@ package plep.web;
 
 import org.json.JSONObject;
 import plep.entite.Utilisateur;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.stream.Collectors;
+
 import static plep.utils.Constantes.UTILISATEUR_BDD;
 
 @WebServlet("/ajouter")
@@ -21,34 +25,45 @@ public class ajouterUtilisateurController extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        JSONObject jObj = new JSONObject("data");
-        System.out.print("caca "+ jObj );
-        /*
-        String data = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        System.out.print("data : " + data);
-        JSONObject jsondata = new JSONObject(data);
-        System.out.println("jsondata : "+jsondata);*/
+        String bouton = request.getParameter("Creer");
+        BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        String jsonString = "";
+
+        jsonString = br.readLine();
+
         JSONObject sendToAjax = new JSONObject();
+        JSONObject receiveJson = null;
+        if (bouton == null) {
+            receiveJson = new JSONObject(jsonString);
+        }
 
-        Utilisateur utilisateur = new Utilisateur();
-        utilisateur.setPrenom(request.getParameter("prenom"));
-        utilisateur.setNom(request.getParameter("nom"));
-        utilisateur.setUsername(request.getParameter("username"));
-        utilisateur.setPassword(request.getParameter("password"));
+        boolean estUtilise = false;
+        if (request.getParameter("username") != null && UTILISATEUR_BDD.usernameAvailable(request.getParameter("username"))) {
+            if (request.getParameter("prenom") != null && request.getParameter("nom") != null
+                    && request.getParameter("username") != null && request.getParameter("password") != null
+                    && request.getParameter("Creer") != null) {
 
-        if (UTILISATEUR_BDD.usernameAvailable(request.getParameter("usernameData"))){
+                Utilisateur utilisateur = new Utilisateur();
+                utilisateur.setPrenom(request.getParameter("prenom"));
+                utilisateur.setNom(request.getParameter("nom"));
+                utilisateur.setUsername(request.getParameter("username"));
+                utilisateur.setPassword(request.getParameter("password"));
 
-            UTILISATEUR_BDD.ajouterUtilisateur(utilisateur);
-            this.getServletContext().getRequestDispatcher("/WEB-INF/view/utilisateur/loginUtilisateur.jsp").forward(request, response);
+                UTILISATEUR_BDD.ajouterUtilisateur(utilisateur);
+                this.getServletContext().getRequestDispatcher("/WEB-INF/view/utilisateur/loginUtilisateur.jsp").forward(request, response);
+            }
+        }
+        if (UTILISATEUR_BDD.usernameAvailable((String) receiveJson.get("usernamedata"))) {
+            estUtilise = false;
         } else {
-            boolean estUtilise = true;
+            estUtilise = true;
+        }
+        if(receiveJson.get("usernamedata")!=null){
             sendToAjax.put("estUtilise", estUtilise);
-            sendToAjax.put("username", request.getParameter("usernameData"));
-            System.out.println(" sendtoajax 2 : " +  sendToAjax);
+            sendToAjax.put("username", receiveJson.get("usernamedata"));
             response.setContentType("application/json");
             PrintWriter writer = response.getWriter();
             writer.append(sendToAjax.toString());
-
         }
     }
 }
