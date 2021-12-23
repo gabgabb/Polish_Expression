3ae24ea829d52562ca5d672f890686022f775cc2
gabgabb <70889534+gabgabb@users.noreply.github.com>
2021-12-23 13:57:35 +0100

    modif profile

--- src/main/java/plep/web/profilUtilisateurController.java
+++ src/main/java/plep/web/profilUtilisateurController.java
@@ -8,12 +8,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import static plep.utils.Constantes.UTILISATEUR_BDD;
 
-@WebServlet("/Profil")
+@WebServlet("/profil")
 public class profilUtilisateurController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        req.getSession().setAttribute("parties",UTILISATEUR_BDD.recupUtilisateur(UTILISATEUR_BDD.getLogUser(req.getSession()).getUsername()));
+        this.getServletContext().getRequestDispatcher("/WEB-INF/view/utilisateur/profileUtilisateur.jsp").forward(req, resp);
     }
 }
 
