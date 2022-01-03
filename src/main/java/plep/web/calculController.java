39ed29ea880bb0d1efe82377758a2c809bd028f6
gabgabb <70889534+gabgabb@users.noreply.github.com>
2022-01-03 20:07:35 +0100

    ajout commentaire + redirection

--- src/main/java/plep/web/calculController.java
+++ src/main/java/plep/web/calculController.java
@@ -20,6 +20,7 @@ public class calculController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        // Getter pour afficher le premier calcul
         int diff = Integer.parseInt((String) req.getSession().getAttribute("difficulte"));
         int nbCalcul = 0;
         int score = 0;
@@ -36,61 +37,64 @@ public class calculController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         int nbCalcul = (Integer) req.getSession().getAttribute("nbCalcul");
         int score = (Integer) req.getSession().getAttribute("Score");
 
-        if (nbCalcul <= 10) {
+        JSONObject sendToAjax = new JSONObject();
+        resp.setContentType("application/json");
+        PrintWriter writer = resp.getWriter();
 
+        // On effectue 10 calculs avant d'afficher le score
+        if (nbCalcul <= 10) {
+            // Permet de récupérer les datas du ajax envoyé
             String data = req.getReader().lines().collect(Collectors.joining());
             JSONObject jsondata = new JSONObject(data);
 
-            JSONObject sendToAjax = new JSONObject();
-
             boolean bonneReponse = false;
 
+            // Vérification pour le premier calcul
             if (nbCalcul == 0) {
 
                 Object reponsePremierCalcul = req.getSession().getAttribute("ReponseCalcul");
 
-                if (Constantes.CALCUL_WEB.verifReponseCalcul((Integer) reponsePremierCalcul, Integer.parseInt((String) jsondata.get("reponse")))) {
+                if (Constantes.CALCUL_WEB.verifReponseCalcul((Double) reponsePremierCalcul, Double.parseDouble((String) jsondata.get("reponse")))) {
                     score++;
-                    System.out.println("SCORE 1: " + score);
                     bonneReponse = true;
                 }
                 nbCalcul++;
+                // Vérification pour les 9 autres calculs
             } else if (nbCalcul != 10) {
 
-                if (Constantes.CALCUL_WEB.verifReponseCalcul((Integer) req.getSession().getAttribute("ReponsePrecedente"), Integer.parseInt((String) jsondata.get("reponse")))) {
+                if (Constantes.CALCUL_WEB.verifReponseCalcul((Double) req.getSession().getAttribute("ReponsePrecedente"), Double.parseDouble((String) jsondata.get("reponse")))) {
                     score++;
                     req.getSession().setAttribute("Score", score);
                     bonneReponse = true;
                 }
                 nbCalcul++;
+                // Affichage du score si dernier calcul a été effectué et redirige à la page profil
                 if (nbCalcul == 10) {
                     sendToAjax.put("score", score);
                     Constantes.PARTIE_BDD.enregistrerScore((Partie) req.getSession().getAttribute("Partie"), score);
+                    sendToAjax.put("redirect", "profil");
                 }
             }
-
+            // Génère le calcul suivant et envoie les informations à l'ajax comme la l'affichage ou la réponse
             if (nbCalcul != 10) {
                 sendToAjax.put("bonneReponse", bonneReponse);
                 Stack calc = Constantes.CALCUL_WEB.GenerationPile(Integer.parseInt((String) req.getSession().getAttribute("difficulte")));
-                int reponseCalcul = Constantes.CALCUL_WEB.resultatCalcul(calc);
+                double reponseCalcul = Constantes.CALCUL_WEB.resultatCalcul(calc);
                 sendToAjax.put("affichageCalcul", Constantes.CALCUL_WEB.afficherCalcul(calc));
                 req.getSession().setAttribute("ReponsePrecedente", reponseCalcul);
             }
+            // Incrémente le nombre de calcul
             req.getSession().setAttribute("nbCalcul", nbCalcul);
             sendToAjax.put("nbCalcul", nbCalcul);
 
-            resp.setContentType("application/json");
-            PrintWriter writer = resp.getWriter();
-
+            // Envoie du json à l'ajax
             writer.append(sendToAjax.toString());
-        } else {
-            resp.sendRedirect("meilleur_score");
         }
     }
-
 }
 
 
