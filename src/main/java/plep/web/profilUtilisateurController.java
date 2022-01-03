6849d88191b4a96f54ef66bc62123285a9907593
gabgabb <70889534+gabgabb@users.noreply.github.com>
2022-01-03 20:11:39 +0100

    profil attribute

--- src/main/java/plep/web/profilUtilisateurController.java
+++ src/main/java/plep/web/profilUtilisateurController.java
@@ -1,11 +1,15 @@
 package plep.web;
 
+import plep.entite.Utilisateur;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import static plep.utils.Constantes.PARTIE_BDD;
 import static plep.utils.Constantes.UTILISATEUR_BDD;
 
 @WebServlet("/profil")
@@ -13,7 +17,15 @@ public class profilUtilisateurController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getSession().setAttribute("parties",UTILISATEUR_BDD.recupUtilisateur(UTILISATEUR_BDD.getLogUser(req.getSession()).getUsername()));
+        // Fixe les variables à afficher sur la page profil selon l'utilisateur
+        Utilisateur user = UTILISATEUR_BDD.getLogUser(req.getSession());
+        req.getSession().setAttribute("parties", UTILISATEUR_BDD.recupPartieUtilisateur(user.getUsername()));
+        req.getSession().setAttribute("nom", UTILISATEUR_BDD.infoUser(user.getUsername()).getNom());
+        req.getSession().setAttribute("prenom", UTILISATEUR_BDD.infoUser(user.getUsername()).getPrenom());
+        req.getSession().setAttribute("username", user.getUsername());
+        req.getSession().setAttribute("nbpartie", PARTIE_BDD.nbPartie(user));
+        req.getSession().setAttribute("moyenne", PARTIE_BDD.calculMoyenne(user));
+
         this.getServletContext().getRequestDispatcher("/WEB-INF/view/utilisateur/profileUtilisateur.jsp").forward(req, resp);
     }
 }
