2d8deff3617a6071a662528df878ef298f7ec500
gabgabb <70889534+gabgabb@users.noreply.github.com>
2022-01-03 20:07:49 +0100

    ajout commentaire

--- src/main/java/plep/calcul/CalculWeb.java
+++ src/main/java/plep/calcul/CalculWeb.java
@@ -6,35 +6,35 @@ import java.util.Stack;
 
 public class CalculWeb {
 
+    // Génère la pile selon une difficulté
     public Stack GenerationPile(int difficulte) {
         return Constantes.EXPRESSION.empile(difficulte);
     }
 
+    // Permet d'afficher le calcul depuis une pile
     public String afficherCalcul(Stack pile) {
 
         Stack cloneCalcul = (Stack) pile.clone();
         Stack pileBonOrdre = new Stack<>();
 
+        // Clone la pile pour la push dans une nouvelle
         while (cloneCalcul.size() > 0) {
             pileBonOrdre.push(cloneCalcul.pop());
         }
-
         return Constantes.EXPRESSION.toStringPile(pileBonOrdre);
-
     }
 
-    public int resultatCalcul(Stack pile) {
+    // Génère le résultat d'une pile
+    public double resultatCalcul(Stack pile) {
         Stack cloneCalcul = (Stack) pile.clone();
-        return Constantes.EXPRESSION.depile(cloneCalcul);
+        return Math.round(Constantes.EXPRESSION.depile(cloneCalcul) * 100) / 100.0;
     }
 
-    public boolean verifReponseCalcul(int resultatPile, int reponse) {
-
+    // Vérifie si la réponse et la le résultat corresponde
+    public boolean verifReponseCalcul(double resultatPile, double reponse) {
         boolean bonneReponse = false;
-
         if (resultatPile == reponse) {
             bonneReponse = true;
-
         }
         return bonneReponse;
     }
